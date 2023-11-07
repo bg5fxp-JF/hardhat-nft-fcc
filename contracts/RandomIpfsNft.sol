96053ff13006197b053f20917fb0a59672504a30
bg5fxp-JF <jacobfecunda@outlook.com>
2023-11-07 11:58:59 +0000

    fixed moddedRng

--- contracts/RandomIpfsNft.sol
+++ contracts/RandomIpfsNft.sol
@@ -91,7 +91,7 @@ contract RandomIpfsNft is VRFConsumerBaseV2, ERC721URIStorage, Ownable {
         uint256 cumulativeSum = 0;
         uint256[3] memory chanceArray = getChanceArray();
         for (uint256 i = 0; i < chanceArray.length; i++) {
-            if (moddedRng >= cumulativeSum && moddedRng < cumulativeSum + chanceArray[i]) {
+            if (moddedRng >= cumulativeSum && moddedRng < chanceArray[i]) {
                 return (Breed(i));
             }
             cumulativeSum += chanceArray[i];
